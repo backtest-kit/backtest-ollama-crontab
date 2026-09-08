ba2ac17d7edf868e35a23cf882c2969547852491
Petr Tripolsky <tripolskypetr@gmail.com>
2026-09-08 20:17:29 +0500

    inc

--- content/jan_2026.strategy/jan_2026.strategy.ts
+++ content/jan_2026.strategy/jan_2026.strategy.ts
@@ -59,7 +59,6 @@ addStrategySchema({
       priceStopLoss: signal.stoploss,
       priceTakeProfit: signal.targets[2],
       minuteEstimatedTime: Infinity,
-      multiplier: 50,
       note: JSON.stringify(info, null, 2),
     };
   },
